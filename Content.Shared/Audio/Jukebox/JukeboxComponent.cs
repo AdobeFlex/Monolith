d9f48f1d288d408de4dbe04b35fc4b428e1e48b8
i_love_Megumin <80097086+R-R0S@users.noreply.github.com>
2026-07-13 19:44:14 +0300

    Add volume slider to jukebox (#399)
    * bugfix
    
    * sec
    
    * oops
    
    * comm

--- Content.Shared/Audio/Jukebox/JukeboxComponent.cs
+++ Content.Shared/Audio/Jukebox/JukeboxComponent.cs
@@ -43,6 +43,11 @@ public sealed partial class JukeboxComponent : Component
     [DataField]
     public Vector2 AudioOffset = Vector2.Zero;
     // End Frontier
+
+    // Exodus-begin Volume slider to jukebox
+    [DataField, AutoNetworkedField]
+    public float Gain = 0.75f;
+    // Exodus-end
 }
 
 [Serializable, NetSerializable]
@@ -84,3 +89,11 @@ public enum JukeboxVisualLayers : byte
 {
     Base
 }
+
+// Exodus-begin Volume slider to jukebox
+[Serializable, NetSerializable]
+public sealed class JukeboxSetGainMessage(float gain) : BoundUserInterfaceMessage
+{
+    public float Gain { get; } = gain;
+}
+// Exodus-end
